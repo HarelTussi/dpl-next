c4257dfc81a49e222560ac246c606d79220f27e8
Harel Tussi <hareltussi@Works-MacBook-Pro.local>
2022-06-08 13:49:30 +0300

    removing unused js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Footer from "@components/Footer";
-import { ClickToComponent } from "click-to-react-component";
 import Header from "@ui/Header";
-import ScrollObserver from "@components/ScrollObserver";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
 import RouteListener from "@components/RouteListener/RouteListener";
 
 const queryClient = new QueryClient();
@@ -13,10 +10,7 @@ const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools />
       <RouteListener />
-      <ScrollObserver />
-      <ClickToComponent />
       <Header />
       <Component {...pageProps} />
       <Footer />
