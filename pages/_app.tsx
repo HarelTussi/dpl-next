c482c3ebd99cedcce44dc7ec30b2bc72f6dc1383
Harel Tussi <hareltussi@Works-MacBook-Pro.local>
2022-06-17 18:05:57 +0300

    adding google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,25 @@ import Footer from "@components/Footer";
 import Header from "@ui/Header";
 import { QueryClient, QueryClientProvider } from "react-query";
 import RouteListener from "@components/RouteListener/RouteListener";
+import Script from "next/script";
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=UA-183371956-1"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-183371956-1');
+        `}
+      </Script>
       <RouteListener />
       <Header />
       <Component {...pageProps} />
