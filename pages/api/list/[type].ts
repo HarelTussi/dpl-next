6d471a26fa3839f5cce36b1a6f44f058f1b2a02d
Harel Tussi <hareltussi@Works-MacBook-Pro.local>
2022-07-04 12:31:17 +0300

    changing list route to ts

--- pages/api/list/[type].ts
+++ pages/api/list/[type].ts
@@ -1,17 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 import { promises as fs } from "fs";
 import path from "path";
 import { asyncCatch } from "../../../utils";
 
-export default async function handler(req, res) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { type } = req.query;
   const dir = path.join(process.cwd(), "public", "list", `${type}.pdf`);
   const [err, file] = await asyncCatch(fs.readFile(dir));
   //@ts-ignore
   if (err) return res.status(500).json({ message: "list not found" });
   res.setHeader("Content-type", "application/pdf");
-  res.setHeader("content-disposition", `attachment; filename=${type}.pdf`);
+  // res.setHeader("content-disposition", `attachment; filename=${type}.pdf`);
   //@ts-ignore
   res.status(200).send(file);
 }
