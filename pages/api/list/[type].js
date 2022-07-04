d471f4f3963835f4a59bfa64f61e3b79df7a4a03
Harel Tussi <hareltussi@Works-MacBook-Pro.local>
2022-07-04 12:21:24 +0300

    changing list route to js

--- pages/api/list/[type].js
+++ pages/api/list/[type].js
@@ -1,13 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+// import type { NextApiRequest, NextApiResponse } from "next";
 import { promises as fs } from "fs";
 import path from "path";
 import { asyncCatch } from "../../../utils";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function handler(req, res) {
   const { type } = req.query;
   const dir = path.join(process.cwd(), "public", "list", `${type}.pdf`);
   const [err, file] = await asyncCatch(fs.readFile(dir));
